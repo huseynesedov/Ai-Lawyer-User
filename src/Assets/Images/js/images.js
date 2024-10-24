bd074186d7c1f88272ea1295c696a2f58315bae4
huseynmamedov <mamedovhuseyn616@gmail.com>
2024-10-24 23:10:28 +0400

    home desing added

--- src/Assets/Images/js/images.js
+++ src/Assets/Images/js/images.js
@@ -2,11 +2,17 @@ import Logo from '../setting 1.svg';
 import Mockup from '../Mockup.svg'
 import google from '../icons8-google.svg';
 import mail from '../Frame 2.svg';
+import homecard1 from '../homecard1.svg'
+import homecard2 from '../homecard2.svg'
+import homecard3 from '../homecard3.svg'
 
 
 export default {
     Logo,
     google,
     mail,
-    Mockup
+    Mockup,
+    homecard1,
+    homecard2,
+    homecard3
 }
